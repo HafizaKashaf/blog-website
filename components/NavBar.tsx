3b77945aa8722ca73fcd703e4abaeebf22e74f24
HafizaKashaf <159634597+HafizaKashaf@users.noreply.github.com>
2025-01-02 21:38:12 +0500

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import { Button, buttonVariants } from "@/components/ui/button";
+import {  buttonVariants } from "@/components/ui/button";
 import { ModeToggle } from './theme-toggle';
 import { Menu } from 'lucide-react';
 import {
